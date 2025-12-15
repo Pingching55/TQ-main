822a5b0ea9baee4c355abc506014940b94fee9eb
pingching <pingching5566@hotmail.com>
2025-12-15 13:20:35 +0800

    update

--- next.config.js
+++ next.config.js
@@ -25,5 +25,17 @@ const nextConfig = {
   },
   // 👆 END OF NEW CONFIGURATION
 };
+ // 👇 ADD THIS WEBPACK CONFIGURATION
+  webpack: (config, { isServer }) => {
+    // We only want to run this customization for the server-side bundle
+    if (isServer) {
+      // These modules are native Node.js dependencies that should NOT be bundled 
+      // into the Vercel Serverless Function (Node.js runtime environment).
+      config.externals.push('bufferutil', 'utf-8-validate');
+    }
+
+    return config;
+  },
+  // 👆 END OF NEW CONFIGURATION
 
 module.exports = nextConfig;
